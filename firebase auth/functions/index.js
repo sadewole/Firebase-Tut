1ee940d0e64363d62790dbd794955bcb668dbc72
Samuel Adewole <samueladewole15@gmail.com>
2019-11-30 02:45:22 +0100

    securing cloud functions

--- firebase auth/functions/index.js
+++ firebase auth/functions/index.js
@@ -3,6 +3,13 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 exports.addAdminRole = functions.https.onCall((data, context) => {
+    // check request is made by an admin
+    if (context.auth.token.admin !== true) {
+        return {
+            error: 'only admins can add other admins, sucker'
+        }
+    }
+
     // get user and add custom claim (admin)
     return admin.auth().getUserByEmail(data.email).then(user => {
         return admin.auth().setCustomUserClaims(user.uid, {
